ef0d37b16e52aae2e0b4d57ba5e4fbd0c7844b13
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-07-13 16:05:31 -0400

    Set up state in your component and adding todos

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useReducer } from "react";
+import { initialState, reducer } from "./reducers/todoReducer";
+import "./App.css";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleChanges = (e) => {
+    setInputValue(e.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={inputValue} onChange={handleChanges} />
+      <button
+        onClick={() => dispatch({ type: "ADD_TODO", payload: inputValue })}
+      >
+        Add Todo
+      </button>
+      <div>
+        {state.map((item) => (
+          <p>{item.item}</p>
+        ))}
+      </div>
     </div>
   );
 }
